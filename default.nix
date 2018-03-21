12e0a8984f6820befdc441dfaa00f0f1c044bf68
Brad Parker <hi@bradparker.com>
2018-03-21 20:02:04 +1000

    Setup for running with Nix

---
+++ default.nix
@@ -0,0 +1,12 @@
+with import <nixpkgs> {};
+{ nixpkgs ? import (fetchgit {
+    inherit (builtins.fromJSON (builtins.readFile ./nixpkgs.json)) url rev sha256;
+  }) {}
+, compiler ? "default"
+}:
+let
+  packages = if compiler == "default"
+    then nixpkgs.haskellPackages
+    else nixpkgs.haskell.packages.${compiler};
+in
+  packages.callPackage ./package.nix { }
